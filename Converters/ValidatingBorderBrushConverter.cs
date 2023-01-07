8078d386d5e032469fa3a4dcf1f029f358430bd6
KaburiCorder <specialemail90@gmail.com>
2023-01-07 21:42:58 +0900

    WpfLib 강의용 소스 게시

---
+++ Converters/ValidatingBorderBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfLib.Converters
+{
+  public class ValidatingBorderBrushConverter : IMultiValueConverter
+  {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+      bool? isValidation = values[0] as bool?;
+      if (isValidation != null && isValidation == true)
+      {
+        return Brushes.Red;
+      }
+      else
+      {
+        return (Brush)values[1];
+      }
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Extensions/ComboBoxExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace WpfLib.Extensions
+{
+  public static class ComboBoxExtensions
+  {
+    public static Border? GetBorder(this ComboBox comboBox)
+    {
+      ToggleButton? toggleButton = (ToggleButton?)comboBox.Template?.FindName("toggleButton", comboBox);
+      return (Border?)toggleButton?.Template?.FindName("templateRoot", toggleButton);
+    }
+
+    public static void SetBorderBrush(this ComboBox comboBox, Brush brush)
+    {
+      Border? border = comboBox.GetBorder();
+      if (border != null)
+        border.BorderBrush = brush;
+    }
+
+    public static void SetBackground(this ComboBox comboBox, Brush brush)
+    {
+      Border? border = comboBox.GetBorder();
+      if (border != null)
+        border.Background = brush;
+
+      TextBox? textbox = (TextBox)comboBox.Template.FindName("PART_EditableTextBox", comboBox);
+      if (textbox != null)
+      {
+        Border? parent = (Border)textbox.Parent;
+        parent.Background = brush;
+      }
+    }
+  }
+}
